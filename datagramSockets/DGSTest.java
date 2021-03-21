48e26dd647866b16a1083f529d3fec3d8887873b
Alessandro Mileto <alessandromileto22@gmail.com>
2021-03-21 16:10:32 +0100

    Test di datagram socket

--- datagramSockets/DGSTest.java
+++ datagramSockets/DGSTest.java
@@ -1,4 +1,40 @@
 package datagramSockets;
 
+import jdk.nashorn.internal.runtime.events.RecompilationEvent;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class DGSTest {
+    private static final int SEND_PORT = 18000, RECEIVE_PORT=20000;
+    public static void main(String[] args) {
+
+        try {
+            DatagramSocket s = new DatagramSocket();
+            System.out.println(s.getPort());
+            InetAddress addr = InetAddress. getByName("localhost");
+            String mex = "Ti spacco a due come finisco di lavorare";
+            byte []bytes = mex.getBytes(StandardCharsets.UTF_8);
+            DatagramPacket dp = new DatagramPacket(bytes, bytes.length, addr, RECEIVE_PORT);
+            int i=4;
+            System.out.println("SENDING");
+            /*System.out.println(s);
+            System.out.println(dp);
+            System.out.println("LOCALHOST -> "+ dp.getAddress());
+            System.out.println(Arrays.toString(dp.getData()));
+            System.out.println(new String(dp.getData()));*/
+            while(i-->0)s.send(dp);
+            s.close();
+        } catch (SocketException e) {
+            System.err.println("SocketExc");
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            System.err.println("Unknown Host");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- datagramSockets/DSServer.java
+++ datagramSockets/DSServer.java
@@ -1,11 +1,36 @@
 package datagramSockets;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 /**
  *
  */
 
 public class DSServer {
-
+    public static void main(String[] args) {
+        try {
+            DatagramSocket ds = new DatagramSocket(20000);
+            System.out.println("PORT CONNECTED --> " + ds.getPort());
+            byte [] buf = new byte[10];
+            DatagramPacket dp = new DatagramPacket(buf, buf.length);
+            int i=4;
+            while(i-->0){
+                ds.receive(dp);
+                System.out.println(dp.getAddress());
+                String msg = new String(buf);
+                System.out.println(msg);
+            }
+            ds.close();
+            System.out.println("BY(T)E");
+        }catch (SocketException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
